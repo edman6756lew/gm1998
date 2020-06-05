60fccacdd4fb3037d4fb58dca0436024dc177c7d
edman6756lew <66494618+edman6756lew@users.noreply.github.com>
2020-06-05 16:36:48 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.google.com/search?sxsrf=ALeKk00HIwDP11PJIG3Dzw4INGrjVEEeag%3A1589956998335&ei=htHEXsz6E8WpmgfHl6WADQ&q=Norton+product+setup&oq=Norton+product+setup&gs_lcp=CgZwc3ktYWIQAzIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIICAAQFhAKEB4yBggAEBYQHjoECAAQRzoECAAQQzoCCAA6BAgAEAo6BQgAEJECUKKvAliZ3QJgpd8CaAFwAXgAgAH1A4gBxjySAQY0LTE2LjGYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab&ved=0ahUKEwjM1d-w68HpAhXFlOYKHcdLCdAQ4dUDCAs&uact=5");
+die();
+?>
